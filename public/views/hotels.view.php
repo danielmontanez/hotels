eaabf50640447d2bc032bd1b9ab8e8ba9ca44382
Daniel M <danielandresmy@hotmail.com>
2018-04-23 14:43:25 -0400

    Hotels retrieved from DB

--- public/views/hotels.view.php
+++ public/views/hotels.view.php
@@ -1,53 +1,52 @@
 <?php
 
 include_once "includes/_header.php"; 
+require $_SERVER['DOCUMENT_ROOT'].'/hotels/core/database/connection.php';
+
+$pdo = Connection::connect();
+$sql="SELECT * FROM hotel;";
+
+$hotels_results=$pdo->prepare($sql);
+$hotels_results->execute();
+
+
+if($hotels_results->columnCount() < 1)
+    echo"<h1>No hotels available</h1>";
+
+?>
+<div class="container">
+  <h1>Hotels</h1>
+    <div class="row">
+        
+        
+        
+        
+<?php
+while ($hotels=$hotels_results->fetch(PDO::FETCH_ASSOC)){
+    
+    $hotel_name = $hotels['hotel_name'];
+    $hotel_desc = $hotels['hotel_desc'];
+    $hotel_image = $hotels['hotel_image'];
+    
+
+    
+    
+echo"
+    <div class='col-md-4'>
+      <div class='thumbnail'>
+          <img src=".$hotel_image." alt=".hotel_name." style='width:100%'>
+          <h3>".$hotel_name."</h3>
+          <div class='caption'>
+            <p>".$hotel_desc."</p>
+          </div>
+      </div>
+    </div>
+    
+    ";
+}
 
 ?>
 
-<div class="container">
-  <h1>Hotels</h1>
-
-  <div class="row">
-    <div class="col-md-4">
-      <div class="thumbnail">
-          <img src="../assets/bestwestern.jpg" alt="Best Western" style="width:100%">
-          <h3>Best Western</h3>
-          <div class="caption">
-            <p>Located in downtown Montreal, with a free parking space and a very cozy ambience. Perfect for family vacations. Breakfast included.</p>
-          </div>
-      </div>
-    </div>
-    <div class="col-md-4">
-      <div class="thumbnail">
-          <img src="../assets/holidayinn.jpg" alt="Holiday Inn" style="width:100%">
-          <h3>Holiday Inn</h3>
-          <div class="caption">
-            <p>Holiday Inn is located in the heart of the historic center of Old Montreal in an extremely quiet and lively area within short walk distance to all sites and is surrounded by the extraordinary beauty of churches.</p>
-          </div>
-      </div>
-    </div>
-    <div class="col-md-4">
-      <div class="thumbnail">
-          <img src="../assets/ledauphin.jpg" alt="Hotel Le Dauphin" style="width:100%">
-          <h3>Hotel Le Dauphin</h3>
-          <div class="caption">
-            <p>The hotel Le Dauphin is the right choice for visitors who are searching for a combination of charm, peace and quiet, and a convenient position from which to explore Montreal.</p>
-          </div>
-      </div>
-    </div>
-    </div>
-      <div class="row">
-
-      <div class="col-md-4">
-      <div class="thumbnail">
-          <img src="../assets/marriott.jpg" alt="Marriott" style="width:100%">
-          <h3>Marriott</h3>
-          <div class="caption">
-            <p>Great for businesses trips, very well located near downtown. Since we try to keep this ambience, kids are not allowed in this hotel.</p>
-          </div>
-      </div>
-    </div>
-      
   </div>
 </div>
 
