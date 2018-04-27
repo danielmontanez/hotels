0dcccb45d6265d354030cb44a5843b7571b8af39
Daniel M <danielandresmy@hotmail.com>
2018-04-27 19:45:42 -0400

    Comments

--- public/actions/signup.php
+++ public/actions/signup.php
@@ -5,12 +5,12 @@ require $_SERVER['DOCUMENT_ROOT'].'/hotels/core/services/SignupProcess.php';
 $signup = new SignUpProcess($_POST['name'], $_POST['email'], $_POST['password'], $_POST['confirm_password']);
 
 if (($signup->validateForm()) && ($signup->userExists())) {
-    //  If form is valid AND the email entered doesn't exist in DB, a new user is created in the database & sends user to view hotels page
+    //  Form is valid AND the email entered doesn't exist in DB, a new user is created in the database & sends user to view hotels page
 	$signup->signUp();
 	header('Location: /hotels/public/views/hotels.view.php');
 	exit();
 } else {
-    // If form isn't valid, returns to sign up page displaying error
+    // Form is invalid, returns to sign up page displaying error
 	header('Location: /hotels/public/views/signup.view.php?error');
 	exit();
 }
