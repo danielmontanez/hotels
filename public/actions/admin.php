501617ff5c634cac0ea44068d2ff2446d4970f18
Haïder Alshamma <haideralsh@gmail.com>
2018-05-04 23:33:56 -0400

    Remove Comments

--- public/actions/admin.php
+++ public/actions/admin.php
@@ -5,11 +5,6 @@ require $_SERVER['DOCUMENT_ROOT'].'/core/models/reservation.php';
 require $_SERVER['DOCUMENT_ROOT'].'/core/services/userService.php';
 require $_SERVER['DOCUMENT_ROOT'].'/core/services/hotelService.php';
 
-// Get reservations
-// Get Hotels and users
-// Make table and wrap with form 
-// Add hidden input for the id
-
 $pdo = Connection::connect();
 $sql = 'SELECT * FROM RESERVATION';
 $stmt = $pdo->prepare($sql);
